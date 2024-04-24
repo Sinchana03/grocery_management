1612033b2d7556b3a72b080ae4cf9e1f905daa22
Sinchana03 <146964048+Sinchana03@users.noreply.github.com>
2024-04-24 21:51:44 +0530

    Update customer.c

--- c_project/customer.c
+++ c_project/customer.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "customer.h"
 
-void getCustomerInfo(struct Customer *customer) {
-    
+Customer* createCustomer(const char *name, const char *phoneNumber, const char *email) {
+    Customer *customer = (Customer*)malloc(sizeof(Customer));
+    if (customer != NULL) {
+        strncpy(customer->name, name, MAX_NAME_LENGTH);
+        strncpy(customer->phoneNumber, phoneNumber, MAX_PHONE_NUMBER_LENGTH);
+        strncpy(customer->email, email, MAX_EMAIL_LENGTH);
+    }
+    return customer;
+}
+
+void displayCustomerInfo(Customer *customer) {
+    if (customer != NULL) {
+        printf("\nCustomer Information:\n");
+        printf("-----------------------------------\n");
+        printf("Name: %s\n", customer->name);
+        printf("Phone Number: %s\n", customer->phoneNumber);
+        printf("Email: %s\n", customer->email);
+        printf("-----------------------------------\n");
+    } else {
+        printf("Error: Customer information is NULL.\n");
+    }
+}
+
+void freeCustomer(Customer *customer) {
+    free(customer);
+}
+
+void getCustomerInfo(Customer *customer) {
     printf("Enter customer name: ");
+    scanf("%s", customer->name);
     fgets(customer->name, MAX_NAME_LENGTH, stdin);
-    strtok(customer->name, "\n");
-    printf("Enter phone number: ");
-    fgets(customer->phoneNumber, 15, stdin);
-    strtok(customer->phoneNumber, "\n");
-    printf("Enter email: ");
+    customer->name[strcspn(customer->name, "\n")] = '\0'; 
+
+    printf("Enter customer phone number: ");
+    fgets(customer->phoneNumber, MAX_PHONE_NUMBER_LENGTH, stdin);
+    customer->phoneNumber[strcspn(customer->phoneNumber, "\n")] = '\0'; 
+
+    printf("Enter customer email: ");
     fgets(customer->email, MAX_EMAIL_LENGTH, stdin);
-    strtok(customer->email, "\n");
+    customer->email[strcspn(customer->email, "\n")] = '\0';
 }
