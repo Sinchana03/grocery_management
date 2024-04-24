86feadbc12aea70a8e9c467880f6dbb7bc739d97
Sinchana03 <146964048+Sinchana03@users.noreply.github.com>
2024-04-24 21:59:11 +0530

    Create linkedlist.c

---
+++ c_project/linkedlist.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "linkedlist.h"
+
+LinkedList* createLinkedList() {
+    LinkedList *list = (LinkedList*)malloc(sizeof(LinkedList));
+    list->head = NULL;
+    list->size = 0;
+    return list;
+}
+
+void append(LinkedList *list, void *data) {
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (list->head == NULL) {
+        list->head = newNode;
+    } else {
+        Node *current = list->head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+
+    list->size++;
+}
+
+void freeLinkedList(LinkedList *list) {
+    Node *current = list->head;
+    while (current != NULL) {
+        Node *temp = current;
+        current = current->next;
+        free(temp);
+    }
+    free(list);
+}
