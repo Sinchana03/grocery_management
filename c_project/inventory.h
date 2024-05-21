593b7ff489b1a2c0c6876c3d8107c54f7b5e8f80
Sinchana03 <146964048+Sinchana03@users.noreply.github.com>
2024-05-21 09:07:50 +0530

    inventory.h

--- c_project/inventory.h
+++ c_project/inventory.h
@@ -1,17 +1,22 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+
 #include "linkedlist.h"
+
 #define MAX_ITEM_NAME_LENGTH 50
 
-typedef struct Item {
+typedef struct {
     char name[MAX_ITEM_NAME_LENGTH];
     float price;
     int quantity;
 } Item;
 
-Item *findItem(LinkedList *list, const char *itemName);
-
 Item* createItem(const char *name, float price, int quantity);
 void displayInventory(LinkedList *inventoryList);
+Item *findItemByName(LinkedList *inventoryList, const char *name);
+void updateItemQuantity(Item *item, int quantity);
+void freeInventory(LinkedList *inventoryList);
+void loadInventoryFromFile(LinkedList *inventoryList);
+void saveInventoryToFile(LinkedList *inventoryList);
 
-#endif // INVENTORY_H
+#endif /* INVENTORY_H */
