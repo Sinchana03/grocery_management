c8a127b6e105eb23dda24881fb7baf508bc968e4
Sinchana03 <146964048+Sinchana03@users.noreply.github.com>
2024-05-21 09:07:09 +0530

    inventory.c

--- c_project/inventory.c
+++ c_project/inventory.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "inventory.h"
-#include "linkedlist.h"
+
+#define INVENTORY_FILE_PATH "C:\\Users\\sinch\\c_grocery_management\\inventory.csv"
 
 Item* createItem(const char *name, float price, int quantity) {
     Item *item = (Item*)malloc(sizeof(Item));
@@ -13,19 +14,6 @@ Item* createItem(const char *name, float price, int quantity) {
     }
     return item;
 }
-
-Item *findItem(LinkedList *list, const char *itemName) {
-    Node *current = list->head;
-    while (current != NULL) {
-        Item *item = (Item *)current->data;
-        if (strcmp(item->name, itemName) == 0) {
-            return item;
-        }
-        current = current->next;
-    }
-    return NULL; 
-}
-
 
 void displayInventory(LinkedList *inventoryList) {
     printf("\nAvailable Items:\n");
@@ -43,7 +31,7 @@ void displayInventory(LinkedList *inventoryList) {
     printf("-----------------------------------\n");
 }
 
-Item* findItemByName(LinkedList *inventoryList, const char *name) {
+Item *findItemByName(LinkedList *inventoryList, const char *name) {
     Node *current = inventoryList->head;
     while (current != NULL) {
         Item *item = (Item*)current->data;
@@ -61,4 +49,53 @@ void updateItemQuantity(Item *item, int quantity) {
 
 void freeInventory(LinkedList *inventoryList) {
     freeLinkedList(inventoryList);
+}
+
+void loadInventoryFromFile(LinkedList *inventoryList) {
+    FILE *file = fopen(INVENTORY_FILE_PATH, "r");
+    if (file == NULL) {
+        perror("Error opening inventory file");
+        printf("Please check file permissions or directory existence: %s\n", INVENTORY_FILE_PATH);
+        exit(EXIT_FAILURE);
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    size_t read;
+
+    while ((read = getline(&line, &len, file)) != -1) {
+    
+        char *name = strtok(line, ",");
+        float price = atof(strtok(NULL, ","));
+        int quantity = atoi(strtok(NULL, ","));
+        Item *item = createItem(name, price, quantity);
+        if (item == NULL) {
+            fprintf(stderr, "Failed to allocate memory for item. Exiting.\n");
+            exit(EXIT_FAILURE);
+        }
+        append(inventoryList, item);
+    }
+
+    if (line) {
+        free(line);
+    }
+    fclose(file);
+}
+
+void saveInventoryToFile(LinkedList *inventoryList) {
+    FILE *file = fopen(INVENTORY_FILE_PATH, "w");
+    if (file == NULL) {
+        perror("Error opening inventory file");
+        printf("Please check file permissions or directory existence: %s\n", INVENTORY_FILE_PATH);
+        exit(EXIT_FAILURE);
+    }
+
+    Node *current = inventoryList->head;
+    while (current != NULL) {
+        Item *item = (Item *)current->data;
+        fprintf(file, "%s,%.2f,%d\n", item->name, item->price, item->quantity);
+        current = current->next;
+    }
+
+    fclose(file);
 }
